d3d61051d5eed0b988dd528f2edd916eb2adc705
yanname <yani.ameziane@ensia.edu.dz>
2025-07-20 22:38:36 +0100

    first

---
+++ chat.py
@@ -0,0 +1,31 @@
+from telegram.ext import * 
+import logging
+
+API_KEY= '8191505882:AAEanKawuSRlxHmt2PsCPaps-ED7FYchnAo'
+
+
+
+async def start_com(update,context):
+   await update.message.reply_text("enter any text to see the biggest in the world")
+
+async def sendthepic_com(update,context):
+   with open(r"karimo.jpg", 'rb') as photo:
+    await update.message.reply_photo(photo=photo)
+
+async def replytohim(update,context):
+  await update.message.reply_text("nta ")
+  with open(r"karimo.jpg",'rb') as photo :
+   await update.message.reply_photo(photo=photo)
+
+if __name__=="__main__":
+  app = ApplicationBuilder().token(API_KEY).build()
+  app.add_handlers(
+    [
+      CommandHandler('start',start_com)
+      ,CommandHandler('send_photo',sendthepic_com)
+      ,MessageHandler(~filters.COMMAND & filters.TEXT , replytohim)
+    ]
+  )
+
+  app.run_polling()
+  
