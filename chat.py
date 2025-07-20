eb1f1423ebea69c1dd6bfeea742ca23947703be7
yanname <yani.ameziane@ensia.edu.dz>
2025-07-20 22:48:03 +0100

    f

--- chat.py
+++ chat.py
@@ -9,20 +9,21 @@ async def start_com(update,context):
    await update.message.reply_text("enter any text to see the biggest in the world")
 
 async def sendthepic_com(update,context):
-   with open(r"karimo.jpg", 'rb') as photo:
+   with open("karimo.jpg", 'rb') as photo:
     await update.message.reply_photo(photo=photo)
 
 async def replytohim(update,context):
   await update.message.reply_text("nta ")
-  with open(r"karimo.jpg",'rb') as photo :
+  with open("karimo.jpg",'rb') as photo :
    await update.message.reply_photo(photo=photo)
 
 if __name__=="__main__":
   app = ApplicationBuilder().token(API_KEY).build()
+  app.add_handler( CommandHandler('start',start_com))
   app.add_handlers(
     [
-      CommandHandler('start',start_com)
-      ,CommandHandler('send_photo',sendthepic_com)
+     
+      CommandHandler('send_photo',sendthepic_com)
       ,MessageHandler(~filters.COMMAND & filters.TEXT , replytohim)
     ]
   )
